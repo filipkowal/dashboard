e7756de78bb34b4fcc06ccd16ae47d0a7404859a
Filip Kowal <filip.kowal@protonmail.com>
2021-02-22 18:54:07 +0100

    Create API calls to add and edit user

--- src/userService.js
+++ src/userService.js
@@ -1,15 +1,41 @@
-const userUrl =
+const url =
   'https://my-json-server.typicode.com/karolkproexe/jsonplaceholderdb/data';
 
 export async function getUsers() {
-  const users = await fetch(userUrl).then(r => r.json());
+  const users = await fetch(url).then(r => r.json());
 
   return users;
 }
 
 export async function deleteUser(userId) {
-  fetch(userUrl + `/${userId}`, {
+  fetch(url + `/${userId}`, {
     method: 'DELETE',
     body: userId,
   });
+}
+
+export async function addUser(user) {
+  const id = Date.now().toString();
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ ...user, id }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then(response => response.json())
+    .then(json => console.log(json));
+}
+
+export async function editUser(user) {
+  fetch(url + `/${user.id}`, {
+    method: 'PUT',
+    body: JSON.stringify(user),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then(response => response.json())
+    .then(json => console.log(json));
 }
