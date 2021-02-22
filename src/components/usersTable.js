a3a771cd7434159d7868e1902362ec683df7409a
Filip Kowal <filip.kowal@protonmail.com>
2021-02-22 18:11:42 +0100

    Display and remove users

--- src/components/usersTable.js
+++ src/components/usersTable.js
@@ -1,20 +1,28 @@
 import { useState, useEffect } from 'react';
 
-import { getUsers } from '../userService';
+import * as api from '../userService';
 
 export default function UsersTable() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    async function fetchUsers() {
-      const users = await getUsers();
+    async function getUsers() {
+      const users = await api.getUsers();
       setUsers(users);
     }
-    fetchUsers();
+    getUsers();
   }, []);
 
   function toggleAddUserForm() {
     console.log('toggleAddUserForm');
+  }
+  function toggleEditUserForm() {
+    console.log('toggleEditUserForm');
+  }
+
+  async function deleteUser(userId) {
+    api.deleteUser(userId);
+    setUsers(users.filter(user => user.id !== userId));
   }
 
   console.log(users);
@@ -24,7 +32,34 @@ export default function UsersTable() {
         <h4>User List</h4>
         <button onClick={toggleAddUserForm}>Add user</button>
       </div>
-      <table></table>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.city}</td>
+              <td>
+                <button onClick={toggleEditUserForm}>Edit</button>
+              </td>
+              <td>
+                <button onClick={() => deleteUser(user.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/userService.js
+++ src/userService.js
@@ -1,7 +1,15 @@
+const userUrl =
+  'https://my-json-server.typicode.com/karolkproexe/jsonplaceholderdb/data';
+
 export async function getUsers() {
-  const users = await fetch(
-    'https://my-json-server.typicode.com/karolkproexe/jsonplaceholderdb/data'
-  ).then(r => r.json());
+  const users = await fetch(userUrl).then(r => r.json());
 
   return users;
+}
+
+export async function deleteUser(userId) {
+  fetch(userUrl + `/${userId}`, {
+    method: 'DELETE',
+    body: userId,
+  });
 }
